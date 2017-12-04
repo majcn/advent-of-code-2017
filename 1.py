f673fd760089385879bd5a5007fdc2f1e2412542
Gregor Majcen <gregor.majcen@sinergise.com>
2017-12-04 12:43:46 +0100

    1st

---
+++ 1.py
@@ -0,0 +1,11 @@
+from PrepareData import PrepareData
+inputdata = PrepareData.getDataAsStr()
+
+
+data = inputdata[0][0]
+l_data = len(data)
+
+
+print sum([int(data[i]) for i in range(l_data) if data[i] == data[(i + 1)          % l_data]])
+
+print sum([int(data[i]) for i in range(l_data) if data[i] == data[(i + l_data / 2) % l_data]])
