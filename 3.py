e5e2a26f231c0ed0f6604cefe65fcb1ef11b1656
Gregor Majcen <gregor.majcen@sinergise.com>
2017-12-29 10:05:14 +0100

    cleanup

--- 3.py
+++ 3.py
@@ -1,7 +1,6 @@
 data = 361527
 
-from math import sqrt
-sdata = int(sqrt(data))
+sdata = int(pow(data, 0.5))
 sdata += 1 if sdata % 2 == 0 else 2
 
 steps = sdata / 2
@@ -11,9 +10,8 @@ print steps + additionalSteps
 
 
 
-
-l = 15
-grid = [[ 0 ] * l for x in range(l)]
+from collections import defaultdict
+grid = defaultdict(int)
 
 up    = lambda x,y: (x    , y - 1)
 down  = lambda x,y: (x    , y + 1)
@@ -21,18 +19,20 @@ right = lambda x,y: (x + 1, y    )
 left  = lambda x,y: (x - 1, y    )
 directions = [right, up, left, down]
 
-x = l / 2
-y = l / 2
+neighbours = lambda x,y: [ (x-1,y-1), (x-1,y+0), (x-1,y+1), (x+0,y-1), (x+0,y+1), (x+1,y-1), (x+1,y+0), (x+1,y+1) ]
+
+x = 0
+y = 0
 directionI = 0
 
-grid[x][y] = 1
-while grid[x][y] < data:
+grid[x,y] = 1
+while grid[x,y] < data:
     newDirectionI = (directionI + 1) % 4
     ndx, ndy = directions[newDirectionI](x, y)
-    if grid[ndx][ndy] == 0:
+    if grid[ndx,ndy] == 0:
         directionI = newDirectionI
 
     x, y = directions[directionI](x, y)
-    grid[x][y] = grid[x-1][y] + grid[x][y-1] + grid[x+1][y] + grid[x][y+1] + grid[x+1][y+1] + grid[x-1][y-1] + grid[x+1][y-1] + grid[x-1][y+1]
+    grid[x,y] = sum([grid[n] for n in neighbours(x,y)])
 
-print grid[x][y]
+print grid[x,y]
